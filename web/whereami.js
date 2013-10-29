88f2e3f347d7b2d33379c60e8bd2af4c0f080c7b
Skye Isard <skye.isard@gmail.com>
2013-10-29 07:38:13 -0500

    Added extra JavaScript resources.

--- web/whereami.js
+++ web/whereami.js
@@ -1,7 +1,24 @@
-var map, locs;
+var locs, map;
 
-function initDBConnection() {
-    locs = new Firebase("https://touch.firebaseio.com/locations")
+function markTrail(points, centerView) {
+    points = points.val();
+    _.each(points, function(pt) {
+	var latLng = new google.maps.LatLng(pt.lat, pt.lon);
+	var marker = new google.maps.MarkerWithLabel({
+	    position: latLng,
+	    title: pt.time,
+	    labelContent: pt.time,
+	    labelClass: "labels", // the CSS class for the label
+	    labelInBackground: false
+	});
+	marker.setMap(map);
+    });
+
+    if (centerView && points.length > 0) {
+	var last = points[points.length - 1];
+	var newCenter = new google.maps.LatLng(last.lat, last.lon);
+	map.setCenter(newCenter);
+    }
 }
 
 function initMap() {
@@ -10,21 +27,19 @@ function initMap() {
 	zoom: 8,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var mapDiv = document.getElementById("map-canvas");
+    map = new google.maps.Map(mapDiv, mapOptions);
     locs.on("value", function(points) {
-	_.each(points.val(), function(pt) {
-	    var latLng = new google.maps.LatLng(pt.lat, pt.lon);
-	    var marker = new google.maps.Marker({
-		position: latLng,
-		title: pt.time
-	    });
-	    marker.setMap(map);
-	})
+	markTrail(points, true);
+	locs.off("value", arguments.callee);
+	locs.on("value", function(changedPoints) {
+	    markTrail(changedPoints, false);
+	});
     })
 }
 
 function init() {
-    initDBConnection();
+    locs = new Firebase("https://touch.firebaseio.com/locations")
     initMap();
 }
 
